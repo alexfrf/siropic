60861367340890cd1a76c1efe11c505b2ead86d3
alexfrf <alexbarco94@gmail.com>
2021-09-18 16:11:30 +0200

    act

--- siro_tw.py
+++ siro_tw.py
@@ -9,16 +9,10 @@ Created on Thu Aug 19 00:54:31 2021
 import pandas as pd
 import numpy as np
 import requests
-from selenium import webdriver # SELENIUM
 from bs4 import BeautifulSoup # BEAUTIFULSOUP
 import time
 import os, os.path
-#from PIL import Image
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import sys
-from selenium.webdriver.common.action_chains import ActionChains
 from github import Github
 #import shutil
 import tweepy
@@ -31,15 +25,6 @@ ACCESS_SECRET = 'ML3s8sj1pEJmjfBF3o3lOVNHcTIt8aITIVpumz81cWvck'
 
 #keyboard = Controller()
 
-"""
-
-pat = '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'
-
-
-github = Github(pat)
-repo = github.get_user().get_repo('siropic')
-files = repo.get_contents(path="Pics")
-"""
 ruta = '/app/Pics/'
 files=[]
 for i in os.listdir(ruta):
@@ -50,22 +35,6 @@ num = np.random.randint(file_count)
 pict = files[num]
 filename = pict
 
-"""
-
-file = repo.get_contents(pict)   
-sfile = "https://raw.githubusercontent.com/alexfrf/siropic/master/{}".format(pict)
-filename = 'temp.jpg'
-r = requests.get(sfile, stream=True)
-if r.status_code == 200:
-    with open(filename, 'wb') as f:
-        for chunk in r:
-            f.write(chunk)
-            
-else:
-    print('Error downloading image')
-    
-"""
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
@@ -75,4 +44,3 @@ status = ""
 imagepath = filename
 
 api.update_with_media(imagepath, status)
-os.remove(filename)
