2eea4422a39b48107285a7cd398566db0c0891d0
alexfrf <alexbarco94@gmail.com>
2021-10-19 21:05:06 +0200

    Encryption

--- siro_tw.py
+++ siro_tw.py
@@ -17,10 +17,10 @@ import tweepy
 
 # Getting our Tweepy keys from an excel file containing our personal tokens
 
-CONSUMER_KEY = CONSUMER_KEY
-CONSUMER_SECRET = CONSUMER_SECRET
-ACCESS_KEY = ACCESS_KEY
-ACCESS_SECRET = ACCESS_SECRET
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 
 
